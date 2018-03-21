8aeea0796524edffb095e1223b2c3f90abdc0660
Stilson <14586012@mstc.edu>
2018-03-20 20:14:22 -0500

    Uploading in class project

--- CameraApp/MainActivity.cs
+++ CameraApp/MainActivity.cs
@@ -1,18 +1,89 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using Android.Provider;
+using System.Collections.Generic;
+using Android.Content.PM;
+using Java.IO;
+using Android.Graphics;
+using System;
+using Android.Runtime;
+using CameraApp;
 
 namespace CameraApp
 {
-    [Activity(Label = "CameraApp", MainLauncher = true)]
+    public static class App
+    {
+        public static File _file;
+        public static File _dir;
+        public static Bitmap bitmap;
+    }
+
+    [Activity(Label = "CameraApp", MainLauncher = true, NoHistory = false)]
     public class MainActivity : Activity
     {
+        ImageView _imageView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+                _imageView = FindViewById<ImageView>(Resource.Id.imageViewCurt);
+
+                button.Click += TakeAPic;
+            }
+        }
+
+        private void TakeAPic(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            App._file = new Java.IO.File(App._dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(App._file));
+            StartActivityForResult(intent, 0);
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            App._dir = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures);
+            if (!App._dir.Exists()) App._dir.Mkdirs();
+        }
+
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities = 
+                PackageManager.QueryIntentActivities(intent, Android.Content.PM.PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            if ( data == null && resultCode == Result.Ok)
+            {
+                Intent mediaIntent = new Intent(Intent.ActionMediaScannerScanFile);
+                Android.Net.Uri contentUri = Android.Net.Uri.FromFile(App._file);
+                mediaIntent.SetData(contentUri);
+                SendBroadcast(mediaIntent);
+
+                int height = Resources.DisplayMetrics.HeightPixels;
+                int width = _imageView.Height;
+                App.bitmap = App._file.Path.LoadAndResizeBitmap(width, height);
+
+                if ( App.bitmap != null)
+                {
+                    _imageView.SetImageBitmap(App.bitmap);
+                    App.bitmap = null;
+                }
+                GC.Collect();
+               
+            }
         }
     }
 }
--- CameraApp/Resources/Resource.Designer.cs
+++ CameraApp/Resources/Resource.Designer.cs
@@ -41,6 +41,28 @@ namespace CameraApp
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040001
+			public const int imageViewCurt = 2130968577;
+			
+			// aapt resource value: 0x7f040000
+			public const int myButton = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int otherButton = 2130968578;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
@@ -62,6 +84,12 @@ namespace CameraApp
 			
 			// aapt resource value: 0x7f030000
 			public const int app_name = 2130903040;
+			
+			// aapt resource value: 0x7f030002
+			public const int imageGallery = 2130903042;
+			
+			// aapt resource value: 0x7f030001
+			public const int openCamera = 2130903041;
 			
 			static String()
 			{
